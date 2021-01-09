271891c029919f966c634c3b351162445edb74c2
vishal saini <vishal.saini@mygurukulam.org>
2021-01-09 16:30:05 +0530

    functions added

---
+++ vars/functions.groovy
@@ -0,0 +1,17 @@
+def cloneRepo() {
+    stage('clone repo') {
+        git 'https://github.com/vishal-cpu/spring3hibernate.git'
+    }
+}
+
+def codeStability() {
+    stage('code stability') {
+        sh 'mvn package'
+    }
+}
+
+def codeQuality() {
+    stage('code quality') {
+        sh 'mvn checkstyle:checkstyle'
+    }
+}
