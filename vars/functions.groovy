ec4176a3d25ac319b41ffd5cc5ea890b76098ff2
vishal saini <vishal.saini@mygurukulam.org>
2021-01-09 23:16:08 +0530

    when condition added

--- vars/functions.groovy
+++ vars/functions.groovy
@@ -1,5 +1,16 @@
 def cloneRepo() {
     stage("clone repo") {
+        when {
+            expression { return enable || disable }
+        }
+        steps {
+            git 'https://github.com/vishal-cpu/spring3hibernate.git'
+        }
+    }
+}
+
+def cloneRepo() {
+    stage("clone repo") {
         git 'https://github.com/vishal-cpu/spring3hibernate.git'
     }
 }
@@ -19,11 +30,5 @@ def codeQuality() {
 def codeCoverage() {
     stage("code coverage") {
         sh 'mvn cobertura:cobertura'
-    }
-}
-
-def codeCoverageReport() {
-    stage('code coverage report') {
-        cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/target/site/cobertura/coverage.xml', conditionalCoverageTargets: '70, 0, 0', failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false
     }
 }
