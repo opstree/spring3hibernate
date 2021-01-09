82abccf7f2cc25841d46346e5bbf267bd776a392
vishal saini <vishal.saini@mygurukulam.org>
2021-01-09 19:33:54 +0530

    added

--- vars/functions.groovy
+++ vars/functions.groovy
@@ -21,3 +21,9 @@ def codeCoverage() {
         sh 'mvn cobertura:cobertura'
     }
 }
+
+def codeCoverageReport() {
+    stage('code coverage report') {
+        cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/target/site/cobertura/coverage.xml', conditionalCoverageTargets: '70, 0, 0', failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false
+    }
+}
