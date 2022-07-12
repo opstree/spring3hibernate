eb987c865b6cfd1971fdee3448292ddf8f8ce4ab
Sandeep <sandeep@opstree.com>
2022-07-12 15:38:00 +0530

    Update default status to scuccess

--- builder/build.sh
+++ builder/build.sh
@@ -9,7 +9,7 @@ echo "Code building done"
 
 OUTPUT_DIR=${EXECUTION_DIR}/${EXECUTION_TASK_ID}
 
-echo '{"mvn_execute": {"status": false, "message": "failed to apply infrastructure via terraform : a null object was created, instead of terraform object"}}' > ${OUTPUT_DIR}/summary.json
+echo '{"mvn_execute": {"status": true, "message": "Dekha chal gaya"}}' > ${OUTPUT_DIR}/summary.json
 
-echo '{"status": false, "message": "failed to apply infrastructure via terraform : a null object was created, instead of terraform object"}' > ${OUTPUT_DIR}/mvn_execute.output
+echo '{"status": true, "message": "Dekha chal gaya"}' > ${OUTPUT_DIR}/mvn_execute.output
 
