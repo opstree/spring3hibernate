55f5177e98182b5cdc78b5df15e29943ebfaa72e
Sandeep <sandeep@opstree.com>
2022-07-12 15:33:31 +0530

    Changes for intergration with BP Step Catalog

--- builder/build.sh
+++ builder/build.sh
@@ -6,3 +6,10 @@ sleep $SLEEP_DURATION
 cd $CODEBASE_DIR
 mvn $INSTRUCTION
 echo "Code building done"
+
+OUTPUT_DIR=${EXECUTION_DIR}/${EXECUTION_TASK_ID}
+
+echo '{"mvn_execute": {"status": false, "message": "failed to apply infrastructure via terraform : a null object was created, instead of terraform object"}}' > ${OUTPUT_DIR}/summary.json
+
+echo '{"status": false, "message": "failed to apply infrastructure via terraform : a null object was created, instead of terraform object"}' > ${OUTPUT_DIR}/mvn_execute.output
+
