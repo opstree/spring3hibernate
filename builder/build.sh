c3c62d9b3e48a91b9eb7a3c5a5de5703ee399882
Sandeep <sandeep@opstree.com>
2022-07-13 16:18:02 +0530

    Basic refactoring

--- builder/build.sh
+++ builder/build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-
-#!/bin/bash
-
-generateOutput() {
-  Task=$1
-  Status=$2
-  Message=$3
-  OUTPUT_DIR=/src/${EXECUTION_DIR}/${EXECUTION_TASK_ID}
-  mkdir -p ${OUTPUT_DIR}
-     echo "{ \"${Task}\": {\"status\": \"${Status}\", \"message\": \"${Message}\"}}"  | jq . > ${OUTPUT_DIR}/summary.json
-     echo "{ \"status\": \"${Status}\", \"message\": \"${Message}\"}"  | jq . > ${OUTPUT_DIR}/${Task}.json
-    }
+source functions.sh
 
 echo "I'll build the code"
 sleep  $SLEEP_DURATION
@@ -19,12 +8,12 @@ cd  $CODEBASE_DIR
 mvn $INSTRUCTION
 if [ $? -eq 0 ]
 then
-generateOutput mvn_execute true "dhek chal gya"
-echo "build sucessfull"
+  generateOutput mvn_execute true "dhek chal gya"
+  echo "build sucessfull"
 elif  [ $? != 0 ]
 then 
-generateOutput mvn_execute false "nhi chala"
-echo "build unsucessfull"
+  generateOutput mvn_execute false "nhi chala"
+  echo "build unsucessfull"
 fi
 
 
