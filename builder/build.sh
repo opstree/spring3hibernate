988f8bac406d2021fc98ce3fe57fc3c26044ca26
neha <neha.sinha@opstree.com>
2022-07-13 13:05:48 +0530

    adding generateoutput in bash.sh

--- builder/build.sh
+++ builder/build.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
+#!/bin/bash
+
+generateOutput() {
+  Task=$1
+  Status=$2
+  Message=$3
+  OUTPUT_DIR=/src/${EXECUTION_DIR}/${EXECUTION_TASK_ID}
+  mkdir -p ${OUTPUT_DIR}
+     echo "{ \"${Task}\": {\"status\": \"${Status}\", \"message\": \"${Message}\"}}"  | jq . > ${OUTPUT_DIR}/summary.json
+     echo "{ \"status\": \"${Status}\", \"message\": \"${Message}\"}"  | jq . > ${OUTPUT_DIR}/${Task}.json
+    }
+
 echo "I'll build the code"
-sleep $SLEEP_DURATION
+sleep  $SLEEP_DURATION
 
-cd $CODEBASE_DIR
+cd  $CODEBASE_DIR
 mvn $INSTRUCTION
-echo "Code building done"
+if [ $? -eq 0 ]
+then
+generateOutput mvn_execute true "dhek chal gya"
+echo "build sucessfull"
+elif  [ $? != 0 ]
+then 
+generateOutput mvn_execute false "nhi chala"
+echo "build unsucessfull"
+fi
 
-OUTPUT_DIR=${EXECUTION_DIR}/${EXECUTION_TASK_ID}
 
-echo '{"mvn_execute": {"status": true, "message": "Dekha chal gaya"}}' > ${OUTPUT_DIR}/summary.json
 
-echo '{"status": true, "message": "Dekha chal gaya"}' > ${OUTPUT_DIR}/mvn_execute.output
+
+
+
+
+
+
+
 
