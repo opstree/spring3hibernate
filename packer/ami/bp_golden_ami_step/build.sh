aa7b28401a83703c1085ea761f142a6aea387459
Surabhi Maheshwari <surabhi95maheshwari@gmail.com>
2022-11-23 16:59:53 +0530

    modified Dockerfile and build script

--- packer/ami/bp_golden_ami_step/build.sh
+++ packer/ami/bp_golden_ami_step/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source /opt/buildpiper/shell-functions/functions.sh
 
-echo "Manage the packer code available at [$WORKSPACE] and have mounted at [${CODEBASE_DIR}/${PACKER_CODE}]"
+echo "Manage the packer code available at [$WORKSPACE] and have mounted at [${CODEBASE_DIR}]"
 sleep  $SLEEP_DURATION
 
 
-cd  ${WORKSPACE}/${CODEBASE_DIR}/${PACKER_CODE}
+cd  ${WORKSPACE}/${CODEBASE_DIR}
 logInfoMessage "packer ${INSTRUCTION} ${EXTRA_VARS}"
 
 packer ${INSTRUCTION} ${EXTRA_VARS}
