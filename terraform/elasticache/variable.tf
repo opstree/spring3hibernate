22820b83e88fc89532e58c1bf765c391ea61e37d
Surabhi Maheshwari <surabhi95maheshwari@gmail.com>
2022-12-01 11:19:48 +0530

    updated variables

--- terraform/elasticache/variable.tf
+++ terraform/elasticache/variable.tf
@@ -1,2 +1,2 @@
-security_group_ids = data.terraform_remote_state.network.outputs.web_sg_id
-subnet_ids = data.terraform_remote_state.network.outputs.public_subnet_ids
+# security_group_ids = data.terraform_remote_state.network.outputs.web_sg_id
+# subnet_ids = data.terraform_remote_state.network.outputs.public_subnet_ids
