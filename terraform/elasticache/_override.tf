c7152b7ad0233d4e18f756363a6bdfe4c18200de
Surabhi Maheshwari <surabhi95maheshwari@gmail.com>
2022-12-02 12:40:33 +0530

    updated override variables with local block

--- terraform/elasticache/_override.tf
+++ terraform/elasticache/_override.tf
@@ -1,15 +1,4 @@
-variable "name" {
-  type        = string
-  default     = "elasticache-cluster"
-  description = "Name of the cluster"
-}
-variable "security_group_ids" {
-  type        = string
-  default     = "${local.default_security_group_ids}"
-  description = "One or more Amazon VPC security groups associated with this replication group. Use this parameter only when you are creating a replication group in an Amazon Virtual Private Cloud"
-}
-variable "subnet_ids" {
-  type        = list(string)
-  default     = "${local.default_subnet_ids}"
-  description = "List of VPC Subnet IDs for the cache subnet group"
+locals {
+  security_group_ids = "${data.terraform_remote_state.network.outputs.web_sg_id}"
+  #subnet_ids = "${data.terraform_remote_state.network.outputs.public_subnet_ids}"
 }
--- terraform/elasticache/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  default_security_group_ids = "${data.terraform_remote_state.network.outputs.web_sg_id}"
-  default_subnet_ids = "${data.terraform_remote_state.network.outputs.public_subnet_ids}"
-}
