e238aa34bf25dabab2e8fccc7c2be6cf7dc225d3
Surabhi Maheshwari <surabhi95maheshwari@gmail.com>
2022-12-02 12:55:06 +0530

    updated override variable

--- terraform/elasticache/_override.tf
+++ terraform/elasticache/_override.tf
@@ -1,4 +1,4 @@
 locals {
   security_group_ids = "${data.terraform_remote_state.network.outputs.web_sg_id}"
-  #subnet_ids = "${data.terraform_remote_state.network.outputs.public_subnet_ids}"
+  subnet_ids = "${data.terraform_remote_state.network.outputs.public_subnet_ids}"
 }
