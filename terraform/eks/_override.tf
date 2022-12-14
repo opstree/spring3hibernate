b9cb330ef32ead734b08657307ab864eac633954
priyanshichauhan0707 <priyanshichauhan0707@gmail.com>
2022-12-14 15:35:47 +0530

    modified:   _override.tf

--- terraform/eks/_override.tf
+++ terraform/eks/_override.tf
@@ -1,5 +1,5 @@
 locals {
   vpc_id = "${data.terraform_remote_state.network.outputs.vpc_id}"
   eks_subnets = "${data.terraform_remote_state.network.outputs.public_subnet_ids}"
-  subnets = "${data.terraform_remote_state.network.outputs.pvt_subnet_ids}"
+  subnets = ["${data.terraform_remote_state.network.outputs.pvt_subnet_ids}"]
 }
