337ca44ba5b05fd6eb07f013016f469c08cbf00a
Deepak Gupta <75674594+deepakgupta97@users.noreply.github.com>
2023-05-09 22:19:00 +0530

    Update data_network.tf

--- terraform/eks/data_network.tf
+++ terraform/eks/data_network.tf
@@ -2,8 +2,8 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket  = "bp-infra"
+    bucket  = "ot-bp-tf-states"
     key     = "network/terraform.tfstate"
-    region  = "us-west-2"
+    region  = "us-west-1"
   }
 }
